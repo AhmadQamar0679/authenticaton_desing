578ddfe2e9e5e10d3b1ec88daf55545bc77c1db4
Ahmad Qamar <ahmadqamar.rq@gmail.com>
2026-05-07 22:15:21 +0500

    .

--- a_main/b_auth/urls.py
+++ a_main/b_auth/urls.py
@@ -6,6 +6,8 @@ from b_auth import views
 urlpatterns = [
     path('',views.add_patient,name='home'),
     path('view_patients/',views.view_patient,name='view_patient'),
+    # -----------------------Add patient Record--------------------
+    path('patient_recode/',views.patient_recode,name='record_page'),
     path('login/',views.login_page,name='login_page'),
     path('register/',views.register_page,name='register'),
     path('logout/',views.logout_page,name='logout')
