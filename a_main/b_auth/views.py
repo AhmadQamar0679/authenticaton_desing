08915a56c5fd4ae082561f05e7b81abc27c1cef1
Ahmad Qamar <ahmadqamar.rq@gmail.com>
2026-05-25 11:12:23 +0500

    .

--- a_main/b_auth/views.py
+++ a_main/b_auth/views.py
@@ -78,17 +78,17 @@ def register_page(request):
         password = request.POST.get('password1')
         confirm_password = request.POST.get('password2')
 
-        # Check both passwords are same
+        # Check both Passwords are same
         if password != confirm_password:
             messages.error(request, 'Passwords do not match')
             return redirect('/register/')
 
-        # Check unique email
+        # Check unique Email
         if User.objects.filter(email=email).exists():
             messages.error(request, 'Email already exists')
             return redirect('/register/')
 
-        # Check unique username
+        # Check unique userName
         if User.objects.filter(username=username).exists():
             messages.error(request, 'Username already exists')
             return redirect('/register/')
